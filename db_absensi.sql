3ac17c0966c03aed15f43ab23a80c744831dcaf7
Zx-Akito <zxakito89@gmail.com>
2022-03-29 11:59:56 +0700

    Add database backup

---
+++ db_absensi.sql
@@ -0,0 +1,88 @@
+/*
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 10.4.20-MariaDB : Database - db_absensi
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_absensi` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `db_absensi`;
+
+/*Table structure for table `absensi` */
+
+DROP TABLE IF EXISTS `absensi`;
+
+CREATE TABLE `absensi` (
+  `id_absensi` int(11) NOT NULL AUTO_INCREMENT,
+  `nis` int(11) DEFAULT NULL,
+  `nama` varchar(30) DEFAULT NULL,
+  `masuk` datetime DEFAULT NULL,
+  `keluar` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_absensi`),
+  KEY `nis` (`nis`),
+  CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `absensi` */
+
+/*Table structure for table `kelas` */
+
+DROP TABLE IF EXISTS `kelas`;
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varbinary(3) DEFAULT NULL,
+  `jurusan` enum('Rekayasa Perangkat Lunak','Teknik Komputer Jaringan','Akuntansi Keuangan Lembaga','Perhotelan I','Perhotelan II','Teknik Kendaraan Ringan Otomotif I','Teknik Kendaraan Ringan Otomotif II','Otomatisasi Tata Kelola Perkantoran I','Otomatisasi Tata Kelola Perkantoran II') DEFAULT NULL,
+  PRIMARY KEY (`id_kelas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `kelas` */
+
+/*Table structure for table `pengguna` */
+
+DROP TABLE IF EXISTS `pengguna`;
+
+CREATE TABLE `pengguna` (
+  `id_pengguna` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) DEFAULT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(99) DEFAULT NULL,
+  PRIMARY KEY (`id_pengguna`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `pengguna` */
+
+insert  into `pengguna`(`id_pengguna`,`nama`,`username`,`password`) values 
+(1,'Admin','admin','$2y$10$3a2KmNqpclVsI0mFvlDU0O38l181nisqM0vYMVL5NhLWsJPbbV2fW');
+
+/*Table structure for table `siswa` */
+
+DROP TABLE IF EXISTS `siswa`;
+
+CREATE TABLE `siswa` (
+  `nis` int(11) NOT NULL,
+  `id_kelas` int(11) DEFAULT NULL,
+  `id_pengguna` int(11) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `password` varchar(99) DEFAULT NULL,
+  PRIMARY KEY (`nis`),
+  KEY `id_pengguna` (`id_pengguna`),
+  KEY `id_kelas` (`id_kelas`),
+  CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`),
+  CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `siswa` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
