e6b8ae6405a1cca419bc6c6606e4d405eca76630
Zx-Akito <zxakito89@gmail.com>
2022-03-29 12:11:58 +0700

    add custom class for change text color

--- pages/beranda.php
+++ pages/beranda.php
@@ -17,7 +17,7 @@
                         <div class='card-body'>
                             <div class='row no-gutters align-items-center'>
                                 <div class='col mr-2'>
-                                    <div class='text-xs font-weight-bold text-primary text-uppercase mb-1'>
+                                    <div class='text-xs font-weight-bold text-blue text-uppercase mb-1'>
                                     Nama sekolah</div>
                                     <div class='h6 mb-0 font-weight-bold text-gray-800'>SMK PASUNDAN SUBANG</div>
                                 </div>
@@ -36,7 +36,7 @@
                         <div class='card-body'>
                             <div class='row no-gutters align-items-center'>
                                 <div class='col mr-2'>
-                                    <div class='text-xs font-weight-bold text-primary text-uppercase mb-1'>
+                                    <div class='text-xs font-weight-bold text-warning text-uppercase mb-1'>
                                     Absen Masuk</div>
                                     <div class='mb-0 font-weight-bold text-gray'>
                                         <p>Total : 90</p>
