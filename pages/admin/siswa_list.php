010da54b44b5d5fc275e6796d625edb52910af88
GTM-Winter <dimastriana03@gmail.com>
2022-03-30 00:03:11 -0700

    add folder admin and file siswa list

---
+++ pages/admin/siswa_list.php
@@ -0,0 +1,45 @@
+<?php
+    $active1="active-link";
+    $template = "dashboard";
+    $title = "Beranda";
+    $konten = "
+
+    <section class='container section animate__animated animate__fadeInLeft' id='home'>
+        <div class='container-fluid d-flex justify-content-between'>
+            <h4>Daftar Siswa</h4>
+            <a href='?page=pemilik/pengguna_tambah' class='d-none d-inline-block btn btn-sm btn-primary shadow-sm'><i class='fas fa-plus fa-2x'></i></a>
+        </div>
+    </section>
+    <div class='container'>
+        <div class='card shadow mb-4'>
+            <div class='card-header py-3'>
+                <h6 class='m-0 font-weight-bold text-primary'>Siswa</h6>
+            </div>
+            <div class='card-body'>
+                <div class='table-responsive'>
+                    <table class='table table-bordered text-center' id='datatable' width='100%' cellspacing='0'>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Kelas</th>
+                            <th>Jurusan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                        <tr>
+                            <td>1</td>
+                            <td>Dimas Triana</td>
+                            <td>Xii</td>
+                            <td>Rekayasa Perangkat Lunak</td>
+                            <td>Edit | Hapus</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    $lihat
+        
+    ";
+?>
