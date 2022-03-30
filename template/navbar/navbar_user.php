872a6dff15301510a20bbc6ff6d8efdba77e19f3
Zx-Akito <zxakito89@gmail.com>
2022-03-30 08:24:34 +0700

    Change value nav item

--- template/navbar/navbar_user.php
+++ template/navbar/navbar_user.php
@@ -4,7 +4,7 @@
         <li class='nav__item $active3'>
             <a href='#contactme' class='nav__link'>
                 <i class='bx bx-bar-chart-alt-2 nav__icon'></i>
-                <span class='nav__name'>Laporan</span>
+                <span class='nav__name'>History</span>
             </a>
         </li>
     
