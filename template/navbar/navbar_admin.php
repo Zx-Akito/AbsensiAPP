0114cdb2f8c1d626b83ae2fa7b8391e707fa5605
GTM-Winter <dimastriana03@gmail.com>
2022-03-30 00:04:51 -0700

    update link and active navbar

--- template/navbar/navbar_admin.php
+++ template/navbar/navbar_admin.php
@@ -1,22 +1,22 @@
 <?php
     $navbar_mode = "
     
-        <li class='nav__item $active1'>
-            <a href='pengguna.html' class='nav__link'>
+        <li class='nav__item'>
+            <a href='?page=admin/siswa_list' class='nav__link $active1'>
                 <i class='bx bx-user nav__icon'></i>
                 <span class='nav__name'>Siswa</span>
             </a>
         </li>
 
-        <li class='nav__item $active2'>
-            <a href='#skills' class='nav__link'>
+        <li class='nav__item'>
+            <a href='#skills' class='nav__link $active2'>
                 <i class='bx bx-spreadsheet nav__icon'></i>
                 <span class='nav__name'>Kelas</span>
             </a>
         </li>
 
-        <li class='nav__item $active3'>
-            <a href='#portfolio' class='nav__link'>
+        <li class='nav__item'>
+            <a href='#portfolio' class='nav__link $active3'>
                 <i class='bx bx-bar-chart nav__icon'></i>
                 <span class='nav__name'>Laporan</span>
             </a>
