5b4e20e477e88f96e0a08c0fe1ec9c784d044ba5
kresna-rev <mahesamaldini79@gmail.com>
2022-04-04 12:29:00 +0700

    Add page Laporan

--- pages/admin/laporan.php
+++ pages/admin/laporan.php
@@ -5,7 +5,44 @@ $template="dashboard";
 $title="Laporan";
 $konten="
 
-Disini Konten
+<section class='container section' id='home'>
+<div class='container-fluid d-flex justify-content-between'>
+    <h4>Data Laporan</h4>
+</div>
+</section>
+<div class='container'>
+<div class='card shadow mb-4'>
+    <div class='card-header py-3'>
+        <h6 class='m-0 font-weight-bold text-primary'>Laporan</h6>
+    </div>
+    <div class='card-body'>
+        <div class='table-responsive'>
+            <table class='table table-bordered text-center' id='datatable' width='100%' cellspacing='0'>
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Kelas</th>
+                    <th>Jurusan</th>
+                    <th>Absen Masuk</th>
+                    <th>Absen Keluar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Ucup</td>
+                    <td>XII</td>
+                    <td>RPL</td>
+                    <td>2022/04/04 07:10am</td>
+                    <td>2022/04/04 12:10am</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
 
 ";
 
