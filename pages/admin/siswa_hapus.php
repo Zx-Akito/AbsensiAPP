1d12f0b15c99b3fd4870474d33e3c0f79c1d76ec
kresna-rev <mahesamaldini79@gmail.com>
2022-04-07 14:29:15 +0700

    Update siswa hapus

--- pages/admin/siswa_hapus.php
+++ pages/admin/siswa_hapus.php
@@ -1,7 +1,8 @@
 <?php
 	$id=$_GET['id'];
-    $h=Hapus("siswa","username='$id'");
-    if ($h) {
+    $h=Hapus("siswa","username_siswa='$id'");
+    $h1=Hapus("pengguna","username='$id'");
+    if ($h and $h1) {
     	header("location:?page=admin/siswa_list&h=true");
     }
     else
