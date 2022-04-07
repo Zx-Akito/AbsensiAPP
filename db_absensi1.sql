ecae219b019d43f0e3ec3d20d7322d8b18162744
kresna-rev <mahesamaldini79@gmail.com>
2022-04-07 13:40:36 +0700

    Update Database

--- db_absensi1.sql
+++ db_absensi1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2022 at 08:38 AM
+-- Generation Time: Apr 07, 2022 at 08:19 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
