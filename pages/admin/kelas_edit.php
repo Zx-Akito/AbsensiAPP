91b0e877f3681cf3d694ee4c488516df150b0a7a
GTM-Winter <dimastriana03@gmail.com>
2022-04-08 09:25:36 +0700

    Update Kelas edit

--- pages/admin/kelas_edit.php
+++ pages/admin/kelas_edit.php
@@ -2,6 +2,57 @@
     $active2="active-link";
     $template = "dashboard";
     $title = "Beranda";
+    $id=$_GET['id'];
+    $tabel="kelas";
+    $kunci="id_kelas='$id'";
+    $tombol=$_POST['tombol'];
+    $var01=$_POST['var01'];
+    $var02=$_POST['var02'];
+
+    if($tombol)
+    {
+        if($var01 AND $var02!="")
+        {
+            $field="
+            nama='$var01',
+            jurusan='$var02'
+            ";
+            $alert=Edit($tabel,$field,$kunci);
+        }
+    }
+
+    $cek=AmbilData($tabel,$kunci);;
+    $cek['jurusan'];
+    switch ($cek['jurusan']) {
+        case 'Rekayasa Perangkat Lunak':
+            $pilih1="selected";
+        break;
+        case 'Teknik Komputer Jaringan':
+            $pilih2="selected";
+        break;
+        case 'Akuntansi Keuangan Lembaga':
+            $pilih3="selected";
+        break;
+        case 'Otomatisasi Tata Kelola Perkantoran I':
+            $pilih4="selected";
+        break;
+        case 'Otomatisasi Tata Kelola Perkantoran II':
+            $pilih5="selected";
+        break;
+        case 'Perhotelan I':
+            $pilih6="selected";
+        break;
+        case 'Perhotelan II':
+            $pilih7="selected";
+        break;
+        case 'Teknik Kendaraan Ringan Otomotif I':
+            $pilih8="selected";
+        break;
+        case 'Teknik Kendaraan Ringan Otomotif II':
+            $pilih9="selected";
+        break;
+    }
+
     $konten ="
     <section class='container section' id='home'>
         <div class='container-fluid d-flex justify-content-between'>
@@ -15,17 +66,22 @@
                     <div class='form-group row pl-2'>
                         <label class='col-sm-1 col-form-label'>Nama</label>
                         <div class='col-sm-5'>
-                            <input type='text' class='form-control form-control-sm' name='var01'>
+                            <input type='text' class='form-control form-control-sm' name='var01'  value='$cek[nama]'>
                         </div>
                     </div> 
                     <div class='form-group row pl-2'>
-                            <label class='col-sm-1 col-form-label'>Jurusan</label>
+                        <label class='col-sm-1 col-form-label'>Jurusan</label>
                         <div class='col-sm-5'>
-                            <select class='custom-select' name='pilih'  id='pilih'>
-                                <option selected>-- Pilih --</option>
-                                <option value='1'>Pemilik</option>
-                                <option value='2'>Gudang</option>
-                                <option value='3'>Kasir</option>
+                            <select class='custom-select' name='var02'  id='pilih'>
+                                <option value='Rekayasa Perangkat Lunak' $pilih1>Rekayasa Perangkat Lunak</option>
+                                <option value='Teknik Komputer Jaringan' $pilih2>Teknik Komputer Jaringan</option>
+                                <option value='Akuntansi Keuangan Lembaga' $pilih3>Akuntansi Keuangan Lembaga</option>
+                                <option value='Otomatisasi Tata Kelola Perkantoran I' $pilih4>Otomatisasi Tata Kelola Perkantoran I</option>
+                                <option value='Otomatisasi Tata Kelola Perkantoran II' $pilih5>Otomatisasi Tata Kelola Perkantoran II</option>
+                                <option value='Perhotelan I' $pilih6>Perhotelan I</option>
+                                <option value='Perhotelan II' $pilih7>Perhotelan II</option>
+                                <option value='Teknik Kendaraan Ringan Otomotif I' $pilih8>Teknik Kendaraan Ringan Otomotif I</option>
+                                <option value='Teknik Kendaraan Ringan Otomotif II' $pilih9>Teknik Kendaraan Ringan Otomotif II</option>
                             </select>
                         </div>
                     </div>
