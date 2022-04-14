b199e2fee543c059a81a45082212a52e839921c2
kresna-rev <mahesamaldini79@gmail.com>
2022-04-14 13:05:44 +0700

    update navbar user

--- template/navbar/navbar_user.php
+++ template/navbar/navbar_user.php
@@ -1,8 +1,8 @@
 <?php
     $navbar_mode = "
 
-        <li class='nav__item $active3'>
-            <a href='?page=siswa/laporan' class='nav__link'>
+        <li class='nav__item'>
+            <a href='?page=siswa/laporan' class='nav__link $active3'>
                 <i class='bx bx-bar-chart-alt-2 nav__icon'></i>
                 <span class='nav__name'>History</span>
             </a>
