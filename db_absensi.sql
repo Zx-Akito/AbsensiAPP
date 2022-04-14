667299c286eec447ef771e0249c96bbad75e0144
kresna-rev <mahesamaldini79@gmail.com>
2022-04-14 12:34:45 +0700

    Update database

--- db_absensi.sql
+++ db_absensi.sql
@@ -1,228 +1,170 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 07, 2022 at 08:38 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.28
+/*
+SQLyog Enterprise v13.1.1 (32 bit)
+MySQL - 10.4.24-MariaDB : Database - db_absensi
+*********************************************************************
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
 
+/*!40101 SET SQL_MODE=''*/;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_absensi` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
---
--- Database: `db_absensi`
---
+USE `db_absensi`;
 
--- --------------------------------------------------------
+/*Table structure for table `absensi` */
 
---
--- Table structure for table `absensi`
---
+DROP TABLE IF EXISTS `absensi`;
 
 CREATE TABLE `absensi` (
-  `id_absensi` int(11) NOT NULL,
+  `id_absensi` int(11) NOT NULL AUTO_INCREMENT,
   `username_siswa` char(13) DEFAULT NULL,
   `id_kelas` int(11) DEFAULT NULL,
   `tanggal` date DEFAULT NULL,
-  `jam_aturan_masuk` time DEFAULT NULL,
-  `jam_aturan_pulang` time DEFAULT NULL,
+  `jam_aturan_masuk` time DEFAULT '07:00:00',
+  `jam_aturan_pulang` time DEFAULT '12:30:00',
   `absensi_masuk` time DEFAULT NULL,
-  `absensi_pulang` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `absensi_pulang` time DEFAULT NULL,
+  PRIMARY KEY (`id_absensi`),
+  KEY `nis` (`username_siswa`),
+  KEY `id_kelas` (`id_kelas`),
+  CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`),
+  CONSTRAINT `absensi_ibfk_2` FOREIGN KEY (`username_siswa`) REFERENCES `siswa` (`username_siswa`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+/*Data for the table `absensi` */
 
---
--- Table structure for table `admin`
---
+insert  into `absensi`(`id_absensi`,`username_siswa`,`id_kelas`,`tanggal`,`jam_aturan_masuk`,`jam_aturan_pulang`,`absensi_masuk`,`absensi_pulang`) values 
+(3,'100',1,'2022-04-14',NULL,NULL,'12:19:09','12:19:09'),
+(4,'111',1,'2022-04-14',NULL,NULL,'12:19:09','12:19:09'),
+(5,'try',1,'2022-04-14',NULL,NULL,'13:09:56','00:00:00'),
+(6,'201',1,'2022-04-14',NULL,NULL,'12:19:09',NULL);
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
 
 CREATE TABLE `admin` (
   `username_admin` char(11) NOT NULL,
-  `nama` varchar(20) DEFAULT NULL
+  `nama` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`username_admin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `admin`
---
+/*Data for the table `admin` */
 
-INSERT INTO `admin` (`username_admin`, `nama`) VALUES
-('101', 'dimas');
+insert  into `admin`(`username_admin`,`nama`) values 
+('101','dimas');
 
--- --------------------------------------------------------
+/*Table structure for table `kelas` */
 
---
--- Table structure for table `kelas`
---
+DROP TABLE IF EXISTS `kelas`;
 
 CREATE TABLE `kelas` (
-  `id_kelas` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(3) DEFAULT NULL,
-  `jurusan` enum('Rekayasa Perangkat Lunak','Teknik Komputer Jaringan','Akuntansi Keuangan Lembaga','Perhotelan I','Perhotelan II','Teknik Kendaraan Ringan Otomotif I','Teknik Kendaraan Ringan Otomotif II','Otomatisasi Tata Kelola Perkantoran I','Otomatisasi Tata Kelola Perkantoran II') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `jurusan` enum('Rekayasa Perangkat Lunak','Teknik Komputer Jaringan','Akuntansi Keuangan Lembaga','Perhotelan I','Perhotelan II','Teknik Kendaraan Ringan Otomotif I','Teknik Kendaraan Ringan Otomotif II','Otomatisasi Tata Kelola Perkantoran I','Otomatisasi Tata Kelola Perkantoran II') DEFAULT NULL,
+  PRIMARY KEY (`id_kelas`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `kelas`
---
+/*Data for the table `kelas` */
 
-INSERT INTO `kelas` (`id_kelas`, `nama`, `jurusan`) VALUES
-(1, 'XII', 'Rekayasa Perangkat Lunak'),
-(2, 'XII', 'Teknik Komputer Jaringan');
+insert  into `kelas`(`id_kelas`,`nama`,`jurusan`) values 
+(1,'XII','Rekayasa Perangkat Lunak'),
+(2,'XII','Teknik Komputer Jaringan');
 
--- --------------------------------------------------------
+/*Table structure for table `pengguna` */
 
---
--- Table structure for table `pengguna`
---
+DROP TABLE IF EXISTS `pengguna`;
 
 CREATE TABLE `pengguna` (
-  `username` int(11) NOT NULL,
+  `username` char(13) NOT NULL,
   `password` varchar(99) DEFAULT NULL,
-  `level` enum('admin','siswa') DEFAULT 'siswa'
+  `level` enum('admin','siswa') DEFAULT 'siswa',
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `pengguna`
---
+/*Data for the table `pengguna` */
 
-INSERT INTO `pengguna` (`username`, `password`, `level`) VALUES
-(1, NULL, 'siswa'),
-(12, NULL, 'siswa'),
-(101, '$2y$10$3a2KmNqpclVsI0mFvlDU0O38l181nisqM0vYMVL5NhLWsJPbbV2fW', 'admin'),
-(111, '$2y$10$HIoLUmA3/BNtPl4tbzONWO959NDLRKtDzd4HI7hbOdUoZYpWtzUFm', 'siswa'),
-(201, '$2y$10$3a2KmNqpclVsI0mFvlDU0O38l181nisqM0vYMVL5NhLWsJPbbV2fW', 'siswa'),
-(22223, NULL, 'siswa'),
-(22224, NULL, 'siswa');
+insert  into `pengguna`(`username`,`password`,`level`) values 
+('1',NULL,'siswa'),
+('101','$2y$10$3a2KmNqpclVsI0mFvlDU0O38l181nisqM0vYMVL5NhLWsJPbbV2fW','admin'),
+('111','$2y$10$HIoLUmA3/BNtPl4tbzONWO959NDLRKtDzd4HI7hbOdUoZYpWtzUFm','siswa'),
+('12',NULL,'siswa'),
+('201','$2y$10$3a2KmNqpclVsI0mFvlDU0O38l181nisqM0vYMVL5NhLWsJPbbV2fW','siswa'),
+('22223',NULL,'siswa'),
+('22224',NULL,'siswa');
 
--- --------------------------------------------------------
+/*Table structure for table `siswa` */
 
---
--- Table structure for table `siswa`
---
+DROP TABLE IF EXISTS `siswa`;
 
 CREATE TABLE `siswa` (
   `username_siswa` char(13) NOT NULL,
   `id_kelas` int(11) DEFAULT NULL,
-  `nama` varchar(50) DEFAULT NULL
+  `nama` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`username_siswa`),
+  KEY `id_kelas` (`id_kelas`),
+  CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `siswa`
---
+/*Data for the table `siswa` */
 
-INSERT INTO `siswa` (`username_siswa`, `id_kelas`, `nama`) VALUES
-('100', 1, 'Kresna'),
-('111', 1, 'Dadan'),
-('201', 1, 'Rifki'),
-('try', 1, 'hu');
+insert  into `siswa`(`username_siswa`,`id_kelas`,`nama`) values 
+('100',1,'Kresna'),
+('111',1,'Dadan'),
+('201',1,'Rifki'),
+('try',1,'hu');
 
--- --------------------------------------------------------
+/*Table structure for table `lap` */
 
---
--- Stand-in structure for view `siswa_list`
--- (See below for the actual view)
---
-CREATE TABLE `siswa_list` (
-`id_kelas` int(11)
-,`username_siswa` char(13)
-,`nama_siswa` varchar(50)
-,`nama_kelas` varchar(3)
-,`jurusan` enum('Rekayasa Perangkat Lunak','Teknik Komputer Jaringan','Akuntansi Keuangan Lembaga','Perhotelan I','Perhotelan II','Teknik Kendaraan Ringan Otomotif I','Teknik Kendaraan Ringan Otomotif II','Otomatisasi Tata Kelola Perkantoran I','Otomatisasi Tata Kelola Perkantoran II')
-);
+DROP TABLE IF EXISTS `lap`;
 
--- --------------------------------------------------------
+/*!50001 DROP VIEW IF EXISTS `lap` */;
+/*!50001 DROP TABLE IF EXISTS `lap` */;
 
---
--- Structure for view `siswa_list`
---
+/*!50001 CREATE TABLE  `lap`(
+ `id_absensi` int(11) ,
+ `nama` varchar(50) ,
+ `kelas` varchar(3) ,
+ `jurusan` enum('Rekayasa Perangkat Lunak','Teknik Komputer Jaringan','Akuntansi Keuangan Lembaga','Perhotelan I','Perhotelan II','Teknik Kendaraan Ringan Otomotif I','Teknik Kendaraan Ringan Otomotif II','Otomatisasi Tata Kelola Perkantoran I','Otomatisasi Tata Kelola Perkantoran II') ,
+ `absensi_masuk` time ,
+ `absensi_pulang` time 
+)*/;
+
+/*Table structure for table `siswa_list` */
+
 DROP TABLE IF EXISTS `siswa_list`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `siswa_list`  AS   (select `kelas`.`id_kelas` AS `id_kelas`,`siswa`.`username_siswa` AS `username_siswa`,`siswa`.`nama` AS `nama_siswa`,`kelas`.`nama` AS `nama_kelas`,`kelas`.`jurusan` AS `jurusan` from (`kelas` join `siswa`) where `kelas`.`id_kelas` = `siswa`.`id_kelas`)  ;
+/*!50001 DROP VIEW IF EXISTS `siswa_list` */;
+/*!50001 DROP TABLE IF EXISTS `siswa_list` */;
 
---
--- Indexes for dumped tables
---
+/*!50001 CREATE TABLE  `siswa_list`(
+ `id_kelas` int(11) ,
+ `username_siswa` char(13) ,
+ `nama_siswa` varchar(50) ,
+ `nama_kelas` varchar(3) ,
+ `jurusan` enum('Rekayasa Perangkat Lunak','Teknik Komputer Jaringan','Akuntansi Keuangan Lembaga','Perhotelan I','Perhotelan II','Teknik Kendaraan Ringan Otomotif I','Teknik Kendaraan Ringan Otomotif II','Otomatisasi Tata Kelola Perkantoran I','Otomatisasi Tata Kelola Perkantoran II') 
+)*/;
 
---
--- Indexes for table `absensi`
---
-ALTER TABLE `absensi`
-  ADD PRIMARY KEY (`id_absensi`),
-  ADD KEY `nis` (`username_siswa`),
-  ADD KEY `id_kelas` (`id_kelas`);
+/*View structure for view lap */
 
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`username_admin`);
+/*!50001 DROP TABLE IF EXISTS `lap` */;
+/*!50001 DROP VIEW IF EXISTS `lap` */;
 
---
--- Indexes for table `kelas`
---
-ALTER TABLE `kelas`
-  ADD PRIMARY KEY (`id_kelas`);
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `lap` AS (select `absensi`.`id_absensi` AS `id_absensi`,`siswa`.`nama` AS `nama`,`kelas`.`nama` AS `kelas`,`kelas`.`jurusan` AS `jurusan`,`absensi`.`absensi_masuk` AS `absensi_masuk`,`absensi`.`absensi_pulang` AS `absensi_pulang` from ((`absensi` join `kelas`) join `siswa`) where `absensi`.`username_siswa` = `siswa`.`username_siswa` and `siswa`.`id_kelas` = `kelas`.`id_kelas` group by `absensi`.`id_absensi`) */;
 
---
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`username`);
+/*View structure for view siswa_list */
 
---
--- Indexes for table `siswa`
---
-ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`username_siswa`),
-  ADD KEY `id_kelas` (`id_kelas`);
+/*!50001 DROP TABLE IF EXISTS `siswa_list` */;
+/*!50001 DROP VIEW IF EXISTS `siswa_list` */;
 
---
--- AUTO_INCREMENT for dumped tables
---
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `siswa_list` AS (select `kelas`.`id_kelas` AS `id_kelas`,`siswa`.`username_siswa` AS `username_siswa`,`siswa`.`nama` AS `nama_siswa`,`kelas`.`nama` AS `nama_kelas`,`kelas`.`jurusan` AS `jurusan` from (`kelas` join `siswa`) where `kelas`.`id_kelas` = `siswa`.`id_kelas`) */;
 
---
--- AUTO_INCREMENT for table `absensi`
---
-ALTER TABLE `absensi`
-  MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `kelas`
---
-ALTER TABLE `kelas`
-  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `pengguna`
---
-ALTER TABLE `pengguna`
-  MODIFY `username` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22225;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `absensi`
---
-ALTER TABLE `absensi`
-  ADD CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`),
-  ADD CONSTRAINT `absensi_ibfk_2` FOREIGN KEY (`username_siswa`) REFERENCES `siswa` (`username_siswa`);
-
---
--- Constraints for table `siswa`
---
-ALTER TABLE `siswa`
-  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
