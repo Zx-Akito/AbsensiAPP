e558c7c79a42137a5522af8a44edf29ef1f55ea9
kresna-rev <mahesamaldini79@gmail.com>
2022-04-14 12:42:22 +0700

    update navbar user

--- template/navbar/navbar_user.php
+++ template/navbar/navbar_user.php
@@ -2,7 +2,7 @@
     $navbar_mode = "
 
         <li class='nav__item $active3'>
-            <a href='#contactme' class='nav__link'>
+            <a href='?page=siswa/laporan' class='nav__link'>
                 <i class='bx bx-bar-chart-alt-2 nav__icon'></i>
                 <span class='nav__name'>History</span>
             </a>
